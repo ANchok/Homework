bd7f2f3dba3a342bbe2efb344be6b717ae04769b
ANchok <loleeyan.mondeek@gmail.com>
2024-11-01 19:00:38 +0500

    redact Horse, Pegasus, Eagle _init_ and methods; used type hints

--- Module_6/module_6_3.py
+++ Module_6/module_6_3.py
@@ -1,33 +1,32 @@
 class Horse:
-    def __init__(self, x_distance=0, y_distance=0, sound='Frr'):
-        self.x_distance = x_distance
-        self.sound = sound
-        super().__init__(y_distance)
+    def __init__(self, x_distance=0, sound='Frr'):
+        self.x_distance: int = x_distance
+        self.sound: str = sound
 
-    def run(self, dx):
-        self.x_distance += int(dx)
+    def run(self, dx: int):
+        self.x_distance += dx
 
 
 class Eagle:
-    def __init__(self, x_distance=0, y_distance=0, sound='I train, eat, sleep, and repeat'):
-        self.y_distance = y_distance
-        self.sound = sound
+    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
+        self.y_distance: int = y_distance
+        self.sound: str = sound
 
-    def fly(self, dy):
-        self.y_distance += int(dy)
+    def fly(self, dy: int):
+        self.y_distance += dy
 
 
 class Pegasus(Horse, Eagle):
-    def __init__(self, x_distance=0, y_distance=0, sound='Frr'):
-        super().__init__(x_distance, y_distance, sound)
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
+    def move(self, dx: int, dy: int):
+        self.run(dx)
+        self.fly(dy)
 
-    def move(self, dx, dy):
-        super().run(int(dx))
-        super().fly(int(dy))
-
-    def get_pos(self):
-        return (self.x_distance, self.y_distance)
+    def get_pos(self) -> tuple:
+        return self.x_distance, self.y_distance
 
     def voice(self):
         print(self.sound)

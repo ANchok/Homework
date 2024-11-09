32c0d68be8fe3a0763036d7c5f3ee4a6b41bcf09
ANchok <loleeyan.mondeek@gmail.com>
2024-11-09 23:50:33 +0500

    init 1

--- Module_7/module_7_2.py
+++ Module_7/module_7_2.py
@@ -1,12 +1,15 @@
 def custom_write(file_name: str, string: list) -> dict:
-    file = open(file_name, 'w', encoding='utf-8')
-    string_position = {}
-    i = 0
+    file = open(file_name, 'r+', encoding='utf-8')
+    string_position: dict = {}
+    num = 0
+    # определяет на какой строке остановилась запись в файле test.txt
+    for line in file:
+        num += 1
+    # запись в словарь
     for item in string:
-        string_position[(i, file.tell())] = item
-        i += 1
+        string_position[(num + 1, file.tell())] = item
+        num += 1
         file.write(item + '\n')
-    print(file.tell())
     file.close()
     return string_position
 
